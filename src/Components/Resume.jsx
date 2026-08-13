28eb2285acd0c7e78d2f1b7aebef6cd40748435b
Deepak negi <dnegi5817@gamil.com>
2026-08-13 15:41:38 +0530

    ad updated resume

--- src/Components/Resume.jsx
+++ src/Components/Resume.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import Terminal from "./windows/Terminal";
-import "/public/RenderCV_sb2nov_Theme.pdf";
+import "/public/resume.pdf";
 const Resume = () => {
 	return (
 		<div>
 			<Terminal>
                 {/* Markdown Resume */}
                 <iframe
-                    src="/RenderCV_sb2nov_Theme.pdf"
+                    src="/resume.pdf"
                     width="100%"
                     height="600px"
                     style={{ border: "none",borderRadius: "12px" }}
