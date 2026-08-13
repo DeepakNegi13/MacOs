5c153fb527cb1d0a5e5d4aa295a74f76813e01d8
Deepak negi <dnegi5817@gamil.com>
2026-08-14 01:13:26 +0530

    add link feature

--- src/Components/Doc.jsx
+++ src/Components/Doc.jsx
@@ -26,20 +26,28 @@ const Doc = () => {
 	let clickOnResume = (e) => {
 		e.preventDefault();
 		setOnResume(!onResume);
-	}
+	};
+
+	const [url, setUrl] = useState("");
+	let clickOnLink = (e) => {
+		e.preventDefault();
+		setUrl(prompt("Enter the URL of the website you want to open:"));
+		window.open(url, "_blank");
+	};
+
 	return (
 		<>
 			{onGit && <Github />}
 			{onNotes && <Notes />}
 			{onSpotify && <Spotify />}
 			{onResume && <Resume />}
+
 			<footer>
 				<a
 					href="/"
 					onClick={(e) => {
 						githubClick(e);
-					}}
-				>
+					}}>
 					<div className="git icon">
 						<img src="/doc-icons/github.svg" alt="" />
 					</div>
@@ -50,7 +58,11 @@ const Doc = () => {
 				<div className="cli icon">
 					<img src="/doc-icons/cli.svg" alt="" />
 				</div>
-				<div className="link icon">
+				<div
+					className="link icon"
+					onClick={(e) => {
+						clickOnLink(e);
+					}}>
 					<img src="/doc-icons/link.svg" alt="" />
 				</div>
 				<div className="mail icon">
@@ -60,19 +72,22 @@ const Doc = () => {
 					className="note icon"
 					onClick={(e) => {
 						clickOnNotes(e);
-					}}
-				>
+					}}>
 					<img src="/doc-icons/note.svg" alt="" />
 				</div>
 
-				<div className="pdf icon" onClick={(e) => {
-					clickOnResume(e);
-				}}>
+				<div
+					className="pdf icon"
+					onClick={(e) => {
+						clickOnResume(e);
+					}}>
 					<img src="/doc-icons/pdf.svg" alt="" />
 				</div>
-				<div className="spotify icon" onClick={(e) => {
-					clickOnSpotify(e);
-				}}>
+				<div
+					className="spotify icon"
+					onClick={(e) => {
+						clickOnSpotify(e);
+					}}>
 					<img src="/doc-icons/spotify.svg" alt="" />
 				</div>
 			</footer>
